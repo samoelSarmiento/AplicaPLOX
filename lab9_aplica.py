6b697509d3e7339e3834660ecd2c81c9de13f2d6
samoelSarmiento <samoel.sarmiento@pucp.pe>
2015-11-12 02:39:53 -0500

    ola

--- lab9_aplica.py
+++ lab9_aplica.py
@@ -39,9 +39,10 @@ csv_salida = csv.writer(salida,lineterminator='\n')
 listInvPalabras = []
 diccPalabra = []
 repeticionesPalabra = []
+palabra_df = []
 numPalListaInvertida = 0
 
-# ["palabra",presencia,repTotal,[[0,2], [1,3]]]
+# ["palabra",presencia,[[0,2], [1,3]]]
 def LlenaListaInvertida(filaPalabra, numDoc):
 	#sin repeticiones
 	sinRepeticiones = list(set(filaPalabra))
@@ -64,42 +65,57 @@ def LlenaListaInvertida(filaPalabra, numDoc):
 			nodoPalabra[2].append(nodoDoc)	
 			repActual = repeticionesPalabra[indice]
 			repeticionesPalabra[indice] = repActual+repeticiones		
+
+#Se le da una lista de palabras para preprocesarla
+def PreProcesaLinea(listaPalabras):
+	for car in punt:
+		listaPalabras = listaPalabras.replace(car,"")
+		listaPalabras = listaPalabras.replace('á','a')
+		listaPalabras = listaPalabras.replace('é', 'e')
+		listaPalabras = listaPalabras.replace('í', 'i')
+		listaPalabras = listaPalabras.replace('ó', 'o')
+		listaPalabras = listaPalabras.replace('ú', 'u')
+		listaPalabras = listaPalabras.replace('ü', 'u')
+		listaPalabras = listaPalabras.replace('-',' ')
+	listaPalabras = listaPalabras.lower()
+	#Aquí removemos los stopwords
+	procesada = []
+ 	vec = listaPalabras.split()
+	procesada = [w for w in vec if not w in cachedStopWords]
+	removido = ' '.join(str(e) for e in procesada)
+	return removido
+
 #PREPROCESAMIENTO
-cantDisciplinasOfertadas = 0
+numeroDocumentos = 0
 reg = re.compile('\S{1,}')#Controla las palabras que contará
 palRep = []
 documentoFiltrado = []
 for row in csv_entrada:
 	rep = []
 	for i in range(3,6):		 				
-		for car in punt:
-			row[i] = row[i].replace(car,"")
-			row[i] = row[i].replace('á','a')
-			row[i] = row[i].replace('é', 'e')
-			row[i] = row[i].replace('í', 'i')
-			row[i] = row[i].replace('ó', 'o')
-			row[i] = row[i].replace('ú', 'u')
-			row[i] = row[i].replace('ü', 'u')
-			row[i] = row[i].replace('-',' ')
-		row[i] = row[i].lower()
-		#Aquí removemos los stopwords
-		procesada = []
- 		vec = row[i].split()
-		procesada = [w for w in vec if not w in cachedStopWords]
-		removido = ' '.join(str(e) for e in procesada)
-		row[i] = removido
+		row[i] = PreProcesaLinea(row[i])
 		#Aquí voy juntando las palabras para contarlas
 		texto = row[i].split()
 		palRep.append(texto)
 	#Aqui procesamos la lista invertida
-	LlenaListaInvertida(row[3].split()+row[4].split()+row[5].split(),cantDisciplinasOfertadas+1)
-	cantDisciplinasOfertadas = cantDisciplinasOfertadas + 1
+	LlenaListaInvertida(row[3].split()+row[4].split()+row[5].split(),numeroDocumentos+1)
+	numeroDocumentos += 1
 	documentoFiltrado.append((row[3]+row[4]+row[5]).split())
 	csv_salida.writerow(row)
 
+#listInvPalabras
+#palabra_df
+#numeroDocumentos
+#formamos el vectos df
+for i in range(0,len(diccPalabra)):
+	nodoListaInvertida = listInvPalabras[i]
+	division = numeroDocumentos/nodoListaInvertida[1]
+	palabra_df.append(math.log10(division))
+####
 listaInvertidaArchivo = open("Lista_Invertida.csv","wt")
 csv_lista = csv.writer(listaInvertidaArchivo,lineterminator='\n')
 csv_lista.writerow(listInvPalabras)
+
 
 #Llevo las palabras extraidas a una "lista de palabras"
 pals = [item for sublist in palRep for item in sublist]
@@ -114,10 +130,6 @@ c = Counter(ma.group() for ma in reg.finditer(newp))
 with open('Contador_de_palabras.csv', 'wb') as f:
     w = csv.writer(f)
     w.writerows(c.items())
-    
-lol = ["Cantidad de Disciplinas Ofertadas:",str(cantDisciplinasOfertadas)]
-csv_salida.writerow(lol)
-
 
 ##======================
 ##PARA EL LABORATORIO 2
@@ -162,20 +174,9 @@ row = listaPalabras
 vecPalabras = []
 
 for i in range(0,len(listaPalabras)):
-	for car in punt:
-		listaPalabras[i] = listaPalabras[i].replace(car,"")
-		listaPalabras[i] = listaPalabras[i].replace('á','a')
-		listaPalabras[i] = listaPalabras[i].replace('é', 'e')
-		listaPalabras[i] = listaPalabras[i].replace('í', 'i')
-		listaPalabras[i] = listaPalabras[i].replace('ó', 'o')
-		listaPalabras[i] = listaPalabras[i].replace('ú', 'u')
-		listaPalabras[i] = listaPalabras[i].replace('ü', 'u')
-		listaPalabras[i] = listaPalabras[i].replace('-',' ')
-	listaPalabras[i] = listaPalabras[i].lower()
-	procesada = [w for w in listaPalabras[i] if not w in punt]
-	#Aquí vuelvo esta lista un string
-	removido = ''.join(str(e) for e in procesada)
+	removido = PreProcesaLinea(listaPalabras[i])
 	vectorPalComparacion.append(removido)
+
 compresionLista = [w for w in vectorPalComparacion if not w in cachedStopWords]
 compresionLista = [w for w in compresionLista if not w == ""]
 #formamos el query del (0,0,0,0,0,0,1,0,0)
@@ -183,23 +184,63 @@ compresionLista = [w for w in compresionLista if not w == ""]
 query = [0]*len(diccPalabra)
 for i in range(0,len(compresionLista)):
 	if diccPalabra.count(compresionLista[i]) != 0:
-		indice = diccPalabra.index(compresionLista[i]
+		indice = diccPalabra.index(compresionLista[i])
 		query[indice] = 1
-print query
 
-
+#documentoFiltrado
+#repeticionesPalabra
+#Formamos la lista de los pesos
+listaDocumentoPesos = []
+for i in range(0,len(documentoFiltrado)):
+	nodoDocumento = [0]*len(diccPalabra)
+	for j in range(0,len(documentoFiltrado[i])):
+		palabra = documentoFiltrado[i][j]
+		if diccPalabra.count(palabra) != 0:
+			indice = diccPalabra.index(palabra)
+			nodoDocumento[indice] = repeticionesPalabra[indice]
+	listaDocumentoPesos.append(nodoDocumento)
 """
-print listInvPalabras[0][0]
-print listInvPalabras[0][1]
-print listInvPalabras[0][2]
+salidaDocVectores = open("DocListaPesos.csv","wt")
+csv_salida = csv.writer(salidaDocVectores,lineterminator='\n')
 
-vectorCantPalComparacion = []
-pos = 0
-for i in range(0,len(vectorPalComparacion)):
-	pos = diccPalabra.index(vectorPalComparacion[i]) #La posicion donde lo encuentra
-	if (diccPalabra.count(vectorPalComparacion[i]) != 0): #Si existe en el diccPalabra
-		listInvPalabras[pos]
-		vectorCantPalComparacion[i] = listInvPalabras[pos]
-
-print vectorCantPalComparacion[0]
+for i in range(0,len(listaDocumentoPesos)):
+	csv_salida.writerow(listaDocumentoPesos[i])
 """
+#palabra_df
+#diccPalabra
+#formamos el vector tf_idf
+vector_tf_idf = []
+for i in range(0,len(listaDocumentoPesos)):
+	nodoVector = []
+	maxVal = max(listaDocumentoPesos[i])
+	pesosDoc = listaDocumentoPesos[i]
+	for j in range(0,len(pesosDoc)):
+		tij = pesosDoc[j]/maxVal
+		df = palabra_df[j]
+		nodoVector.append(tij*df)
+	vector_tf_idf.append(nodoVector)
+"""
+salidaDocVectores = open("DocListaVectorTF_IDF.csv","wt")
+csv_salida = csv.writer(salidaDocVectores,lineterminator='\n')
+
+for i in range(0,len(vector_tf_idf)):
+	csv_salida.writerow(vector_tf_idf[i])
+"""
+#calculamos el tf_idf para el query, el query esta en binario asi que el tf solo sera 0 o 1
+vector_query = []
+for i in range(0,len(query)):
+	tij = query[i]
+	df = palabra_df[i]
+	vector_query.append(tij*df)
+
+#preparamos el calculo del ranking
+normaQuery = modulo(vector_query)
+resultados = []
+
+for i in range(0,len(vector_tf_idf)):
+	vector_documento = vector_tf_idf[i]
+	cosine = ppunto(vector_documento,vector_query)/(modulo(vector_documento)*normaQuery)
+	resultados.append(cosine)
+ranking = sorted(range(len(resultados)),key=lambda k : s[k])
+top20 = ranking[:20]
+print top20
